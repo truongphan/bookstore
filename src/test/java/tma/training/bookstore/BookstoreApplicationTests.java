ddcb090a0403ee11a2e2eae2819f939437e748be
Truong Phan <vantruong0810@gmail.com>
2021-10-01 11:34:35 +0700

    initial

---
+++ src/test/java/tma/training/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package tma.training.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
